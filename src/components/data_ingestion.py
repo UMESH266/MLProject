8294264b5c04807350995e530d82efe0cb573935
UMESH266 <umeshgouda143@gmail.com>
2024-04-02 17:21:15 +0530

    data ingetion creation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,9 +1,10 @@
 import os
 import sys
-from src.exception import logging
+from exception import CustomeException
+from logger import logging
 import pandas as pd
 
-from sklearn.model_selecion import train_test_split
+from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 @dataclass
@@ -19,4 +20,30 @@ class DataIngestion:
     def inititate_data_ingestion(self):
         logging.info("Entered the data ingestion method of component")
         try:
-            df = pd.read_csv('')
+            df = pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info('Trian test split initiated')
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            logging.info('Ingestion of te data is completed')
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
+        
+        except Exception as e:
+            raise CustomeException(e, sys)
+
+
+if __name__ == '__main__':
+    obj = DataIngestion()
+    obj.inititate_data_ingestion()
