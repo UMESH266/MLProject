1d89f7b67a83b59f9ebc988a4b08168737af4f90
UMESH266 <umeshgouda143@gmail.com>
2024-04-04 12:46:16 +0530

    data ingetion updation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,7 @@
 import os
 import sys
-from exception import CustomeException
-from logger import logging
+from src.exception import CustomeException
+from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -27,7 +27,7 @@ class DataIngestion:
 
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
-            logging.info('Trian test split initiated')
+            logging.info('Train test split initiated')
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
